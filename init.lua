d724fca117e7803360315e11847096e0f827752b
ptumulty <ptumulty1@gmail.com>
2023-06-18 21:59:44 -0700

    Added shift tab remap

--- init.lua
+++ init.lua
@@ -1,1 +1,11 @@
 require("custom.set")
+
+local function map(mode, lhs, rhs, opts)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+map('i', '<S-TAB>', '<C-d>')
